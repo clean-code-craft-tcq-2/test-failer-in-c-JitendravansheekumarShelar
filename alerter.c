a3279bb036696751591a816c28593c0c58b4a223
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-02-01 23:42:53 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -19,6 +19,28 @@ int networkAlertStub(float celcius) {
         return 500;
 }
 
+void testalertInCelcius(float farenheit) {
+    float celcius = (farenheit - 32) * 5 / 9;
+    int returnCode = networkAlertStub(celcius);
+    static int countCheck = 0;
+    if(celcius < 500)
+        assert (returnCode == 200);
+    else
+    {
+        assert (returnCode == 500);
+        countCheck += 1;
+    }
+        
+    if (returnCode != 200) {
+        // non-ok response is not an error! Issues happen in life!
+        // let us keep a count of failures to report
+        // However, this code doesn't count failures!
+        // Add a test below to catch this bug. Alter the stub above, if needed.
+        alertFailureCount += 1;
+    }
+    assert (countCheck == alertFailureCount);
+}
+
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
     int returnCode = networkAlertStub(celcius);
@@ -27,16 +49,21 @@ void alertInCelcius(float farenheit) {
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+        alertFailureCount += 1;
     }
 }
 
 int main() {
     #if (SW_ENVIRONMENT == SW_TEST_ENVIRONMENT)
+    testalertInCelcius(303.6);
+    testalertInCelcius(400.5);
+    testalertInCelcius(950);
+    testalertInCelcius(1050);
+    #else
     alertInCelcius(400.5);
     alertInCelcius(303.6);
     alertInCelcius(950);
-    assert(alertFailureCount == 1);
+    alertInCelcius(1050);
     #endif
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
