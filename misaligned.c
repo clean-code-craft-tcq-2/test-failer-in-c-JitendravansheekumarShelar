3ec1be31fb660c3815390de80b76070c16f632c8
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-02-01 23:12:35 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -2,16 +2,38 @@
 #include <assert.h>
 
 #define Max_CountInEachColorTable 5U
+#define SW_TEST_ENVIRONMENT 0
+#define SW_PROD_ENVIRONMENT 1
 
-void printOnConsole(int pairNumber , const char* majorColor , const char* minorColor)
+#define SW_ENVIRONMENT SW_TEST_ENVIRONMENT
+
+
+void testprintOnConsole(int pairNumber , const char* majorColor , const char* minorColor)
 {
     const char* testMajorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* testMinorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     
     assert( testMajorColor[pairNumber / Max_CountInEachColorTable] == majorColor);
     assert( testMinorColor[pairNumber % Max_CountInEachColorTable] == minorColor);
-     
-    printf("%d | %s | %s\n", pairNumber , majorColor, minorColor);
+}
+
+int testprintColorMap() {
+
+    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    
+    int i = 0, j = 0;
+    for(i = 0; i < 5; i++) {
+        for(j = 0; j < 5; j++) {
+            testprintOnConsole(i * 5 + j, majorColor[i], minorColor[j]);
+        }
+    }
+    assert((i * j) == 25);
+}
+
+void printOnConsole(int pairNumber , const char* majorColor , const char* minorColor)
+{
+    printf("%d \t| \t%s \t| \t%s\n", pairNumber , majorColor, minorColor);
 }
 
 int printColorMap() {
@@ -20,15 +42,22 @@ int printColorMap() {
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printOnConsole(i * 5 + j, majorColor[i], minorColor[i]);
+            printOnConsole(i * 5 + j, majorColor[i], minorColor[j]);
         }
     }
     return i * j;
 }
 
 int main() {
-    int result = printColorMap();
-    assert(result == 25);
+    
+    #if (SW_ENVIRONMENT == SW_TEST_ENVIRONMENT)
+    int (*fp_printColorMap)(void) = &testprintColorMap;
+    #else
+    int (*fp_printColorMap)(void) = &printColorMap;
+    #endif
+    
+    int result = fp_printColorMap();
+    printf("The %d-Pair Color code is printed succesfully\n",result);
     printf("All is well (maybe!)\n");
     return 0;
 }
